f006b4c875e0289ef498a9d274b074fe36340f5e
Mark <133434946+merk1471@users.noreply.github.com>
2024-02-23 12:16:42 -0500

    Update processes.py

--- processes.py
+++ processes.py
@@ -1,19 +1,19 @@
 import os
-import shutil
+from pathlib import Path
 
 #Get path to downloads and desktop directories
 DOWNLOADPATH = os.path.join(os.path.expanduser("~"), 'Downloads')
 DESKTOPPATH = os.path.join(os.path.expanduser("~"), 'Desktop') 
 
+#function to split file name
 def get_file_options(file_path, option):
     base_name, extension = os.path.splitext(file_path)
     if (option == "ext"):
         return extension
     elif (option == "base"):
         return base_name
-    elif (option == 'both'):
-        return (base_name + extension)
-
+    
+#Function to create directories
 def create_directory_in_desktop(directory_path):
     # Create the directory if it doesn't exist
     newDirPath = os.path.join(DESKTOPPATH, directory_path)
@@ -23,6 +23,7 @@ def create_directory_in_desktop(directory_path):
     else:
         print(f"Directory '{directory_path}' already exists.")
 
+#Find scattered desktop files (not in a folder)
 def find_files_on_desktop():
     # Get the path to the user's desktop directory
     desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
@@ -39,3 +40,41 @@ def find_files_on_desktop():
             files_on_desktop.append(item)
     
     return files_on_desktop
+
+#Find scattered download files (not in a folder)
+def find_files_in_downloads():
+    # Get the path to the user's desktop directory
+    
+    # Initialize a list to store file names
+    download_files = []
+    
+    # Iterate through all items on the desktop
+    for item in os.listdir(DOWNLOADPATH):
+        # Construct the full path for the item
+        item_path = os.path.join(DOWNLOADPATH, item)
+        # Check if the item is a file
+        if os.path.isfile(item_path):
+            download_files.append(item)
+    
+    return download_files
+
+def renameDuplicates(path):
+    delete = os.path.join(DESKTOPPATH, 'other/delete')
+    os.path.join(path, delete)
+ 
+    stem = path.stem
+    suffix = path.suffix
+    counter = 1
+
+    newPath = path
+
+    while newPath.exists():
+        new_name = f"{stem} ({counter}){suffix}"
+        os.rename(path, new_name)
+        newPath = os.path.join(delete, new_name)
+        counter += 1
+
+
+    return str(newPath)
+
+
