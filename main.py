168eb9822a026f7bd228efd4b9a0541e1fc0753d
Mark <133434946+merk1471@users.noreply.github.com>
2024-02-23 12:16:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import shutil
 #Get path to downloads and desktop directories
 DOWNLOADPATH = os.path.join(os.path.expanduser("~"), 'Downloads')
 DESKTOPPATH = os.path.join(os.path.expanduser("~"), 'Desktop') 
+print(DOWNLOADPATH, DESKTOPPATH)
 
 #Find scattered files
 desktopFiles = find_files_on_desktop()
@@ -13,18 +14,25 @@ downloadFiles = find_files_in_downloads()
 
 #Organize desktop files
 for file in desktopFiles:
+    print(file)
     extension = get_file_options(file, "ext")
     if (extension in extension_paths):
         newDir = extension_paths[extension]
         newDirPath = os.path.join(DESKTOPPATH, newDir)
         newFilePath = os.path.join(DESKTOPPATH, file)      
+
         if not os.path.exists(newDirPath):
             create_directory_in_desktop(newDir)
-        #duplicate_checker(file, newDirPath)
-        shutil.move(newFilePath, newDirPath)
+        if (os.path.exists(os.path.join(newDirPath, file))):
+            delete = os.path.join(DESKTOPPATH, 'other/delete')
+            renameDuplicates(newFilePath)
+            shutil.move(newFilePath, delete)
+        else:
+            shutil.move(newFilePath, newDirPath)
     
 #Organize download files
 for file in downloadFiles:
+    print(file)
     extension = get_file_options(file, "ext")
     if (extension in extension_paths):
         newDir = extension_paths[extension]
@@ -32,8 +40,9 @@ for file in downloadFiles:
         newFilePath = os.path.join(DOWNLOADPATH, file)      
         if not os.path.exists(newDirPath):
             create_directory_in_desktop(newDir) 
-        shutil.move(newFilePath, newDirPath)
-        
-
-    
-    
+        if (os.path.exists(os.path.join(newDirPath, file))):
+            delete = os.path.join(DESKTOPPATH, 'other/delete')
+            renameDuplicates(newFilePath)
+            shutil.move(newFilePath, delete)
+        else:
+            shutil.move(newFilePath, newDirPath)
